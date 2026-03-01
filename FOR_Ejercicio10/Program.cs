4d5033bd4c0649f39e49f827c2b2e62d36587c8c
dgTr3 <Edugta31@gmail.com>
2026-03-01 14:30:51 -0600

    completo

--- FOR_Ejercicio10/Program.cs
+++ FOR_Ejercicio10/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Mostrar un patrón numérico");
+for (int i = 1; i <= 4; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(i);
+    }
+    Console.WriteLine();
+}
